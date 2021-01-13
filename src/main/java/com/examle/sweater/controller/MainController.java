a928f4d4817a61e567ee06dfecac6c426de35cb5
Александра Медведева <Sa-medved@yandex.ru>
2021-01-13 17:21:47 +0700

    Добавила регистрацию

--- src/main/java/com/examle/sweater/controller/MainController.java
+++ src/main/java/com/examle/sweater/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.examle.sweater;
+package com.examle.sweater.controller;
 
 import com.examle.sweater.domain.Message;
 import com.examle.sweater.repos.MessageRepos;
@@ -13,24 +13,24 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
-public class GreetingController {
+public class MainController {
     @Autowired
     private MessageRepos messageRepos;
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Map<String, Object> model) {
-        model.put("name", name);
+    @GetMapping("/")
+    public String greeting( Map<String, Object> model) {
+
         return "greeting";
     }
 
-    @GetMapping
+    @GetMapping("/main")
     public String main(Map<String, Object> model) {
         Iterable<Message> messages = messageRepos.findAll();
         model.put("messages", messages);
         return "main";
     }
 
-    @PostMapping
+    @PostMapping("/main")
     public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
         Message message = new Message(text, tag);
         messageRepos.save(message);
